b79455fb76bfcc4f0a748e762fbc56fccd817805
carenalga <mapedorr@gmail.com>
2020-12-19 21:34:44 -0500

    Separated target scene into Begin and End.
    The *-End scenes will be used to show the goal of the video.
    The *-Start scenes will be used to show the process.
    Added a color modulation to the Label to show how the value of the
    method can be used for other things.

--- src/Tween/TweenInterpolateMethod-End/TweenInterpolateMethod.gd
+++ src/Tween/TweenInterpolateMethod-End/TweenInterpolateMethod.gd
@@ -4,12 +4,13 @@ extends MarginContainer
 func _ready():
 	$Tween.interpolate_method(
 		self, 'update_label',
-		0, 50,
-		5, Tween.TRANS_LINEAR, Tween.EASE_OUT,
-		2
+		0, 100,
+		5.0, Tween.TRANS_SINE, Tween.EASE_OUT,
+		2.0
 	)
 	$Tween.start()
 
-
 func update_label(value: float) -> void:
 	$Label.text = str(int(value))
+#	$Label.modulate.b = ((50 - value) / 100) * 2
+#	$Label.modulate.r = ((50 - value) / 100) * 2
